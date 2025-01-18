c0fa257e73f9f3e61dc2155052f3da310b3e0613
Guillaume Lempereur <gui@vps-889fab22.vps.ovh.net>
2025-01-18 13:13:55 +0000

    p184 added and bugged

---
+++ p184/p184.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <iterator>
+#include <string>
+#include <format>
+#include <algorithm>
+#include <sstream>
+
+inline int dst(int x, int y, int d){
+	return x*x+y*y >= d*d;
+}
+
+//p = ay-ax*(ay-by)/(ax-bx)
+inline float s(int ax, int ay, int bx, int by){
+	if(ax == bx){
+		std::cout << "ERROR!" << std::endl;
+		return 0;
+	}
+	float r = (float)(ay)-(float)(ax*(ay-by))/(float)(ax-bx);
+	if(r<0.0001 && r > 0.0001)
+		std::cout << "ERROR 0 !" << std::endl;
+	return r;
+}
+int main(int args, char **argv){
+	/*
+	 * p184 Brute Force
+	 *
+	 * size 2, 3 ok
+	 *
+	 * y = mx+p
+	 * p = ay-ax*(ay-by)/(ax-bx)
+	 *
+	 * size: 2 : 8
+	 * size: 3 : 360
+	 * size: 4 : 
+	 * size: 5 : 2656 wrong : => 10600
+	 *
+	 * size: 10: 
+	 *
+	 * size: 15: 
+	 * size: 20: 
+	 *
+	 * size: 105: 
+	 *
+	*/
+	
+	/* CASE 1:	Ax = Bx (vertical side)
+	 *			Ay > By
+	 *			Cx > 0
+	 *			Ay >= Cy
+	 *			Ax < 0
+	 *			Ay > 0
+	 *
+	 *			AC above (0,0)
+	 *			ay*cx>ax*cy <=> cy > ay*cx/ax
+	 *
+	 *			BC below (0,0)
+	 *			by*cx<bx*cy <=> cy < by*cx/bx
+	 *
+	 *	CASE 2: Ax < Bx (no vertical side)
+	 *			Ay > By
+	 *			Ay > Cy
+	 *			Ax < Cx
+	 *			Bx < Cx and Cx > 0
+	 *			Ax < 0
+	 *			Ay > 0
+	 *
+	 *			subcase 1: By > Cy => BC above (0;0) AC below (0;0)
+	 *			ay*cx>ax*cy <=> cy < ay*cx/ax
+	 *			by*cx<bx*cy <=> cy > by*cx/bx
+	 *
+	 *			subcase 2: By < Cy => BC below (0;0) AC above (0;0)
+	 *			ay*cx>ax*cy <=> cy > ay*cx/ax
+	 *			by*cx<bx*cy <=> cy < by*cx/bx
+	 *
+	*/
+
+	std::set<std::string> set;
+	//std::cout << std::endl;
+	int lim = 5, cnt = 0;
+	if(args==2)
+		lim = std::atoi(argv[1]);
+
+	for(int ax=1-lim;ax<lim;++ax){
+		for(int ay=1-lim;ay<lim;++ay){
+			if(dst(ax, ay, lim))
+				break;
+			for(int bx=1-lim;bx<lim;++bx){
+				for(int by=1-lim;by<lim;++by){
+					if(dst(bx, by, lim))
+						break;
+					for(int cx=1-lim;cx<lim;++cx){
+						for(int cy=1-lim;cy<lim;++cy){ // might be problem !
+							if(dst(cx, cy, lim))
+								break;
+							// check (0;0) in the middle
+							
+							if(ax == bx){ // AB vertical
+								//check AC above 0 and BC below 0 or the opposite
+								if(ax==cx)
+									continue; // aligned => no triangle
+								if(ax*cx>=0) // A and C must be apart from (0;0)
+									continue;
+								float p_bc = s(bx, by, cx, cy);
+								float p_ac = s(ax, ay, cx, cy);
+								if(p_bc*p_ac>=0) // BC below && AC above or the opposite
+									continue;
+							}else if(bx == cx){ // BC vertical
+								//check AC above 0 and BC below 0 or the opposite
+								if(ax*cx>=0) // A and C must be apart from (0;0)
+									continue;
+								float p_ab = s(ax, ay, bx, by);
+								float p_ac = s(ax, ay, cx, cy);
+								if(p_ab*p_ac>=0) // AB below && AC above or the opposite
+									continue;
+							}else if(ax == cx){ // AC vertical
+								//check AC above 0 and BC below 0 or the opposite
+								if(bx*cx>=0) // B and C must be apart from (0;0)
+									continue;
+								float p_ab = s(ax, ay, bx, by);
+								float p_bc = s(bx, by, cx, cy);
+								if(p_ab*p_bc>=0) // AB below && AC above or the opposite
+									continue;
+							}else{//no vertical side
+								float p_ab = s(ax, ay, bx, by);
+								float p_ac = s(ax, ay, cx, cy);
+								float p_bc = s(bx, by, cx, cy);
+								// y = m*x+p
+								float ab_c_y = (float)(ay-by)/(float)(ax-bx)*cx+p_ab;// y of ab line @ cx
+								float ac_b_y = (float)(ay-cy)/(float)(ax-cx)*bx+p_ac;// y of ab line @ bx
+								float bc_a_y = (float)(by-cy)/(float)(bx-cx)*ax+p_bc;// y of ab line @ ax
+
+								/*
+								if(ay==0&&ax==-1&&bx==0&&by==1&&cx==1&&cy==-1){
+									std::cout << p_ab << " " << p_ac << " " << p_bc << std::endl;
+									std::cout << ab_c_y << " " << ac_b_y << " " << bc_a_y << std::endl;
+								}*/
+								if((cy-ab_c_y)*p_ab>=0)//zero not between them
+									continue;
+
+								if((by-ac_b_y)*p_ac>=0)//zero not between them
+									continue;
+
+								if((ay-bc_a_y)*p_bc>=0)//zero not between them
+									continue;
+							}
+							//if(ty*uy>=0) // top & bottom apart from (0;0)
+							//	continue;
+
+							std::vector<std::string> v;
+							v.push_back(std::format("({};{})", ax, ay));
+							v.push_back(std::format("({};{})", bx, by));
+							v.push_back(std::format("({};{})", cx, cy));
+							std::sort(v.begin(), v.end());
+							set.insert(v[0]+v[1]+v[2]);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	std::cout << "size "<<  lim << " : " << set.size() << std::endl;
+
+	/*
+	for(std::set<std::string>::const_iterator it = set.begin();it!=set.end();++it){
+		std::cout << *it << std::endl;
+	}*/
+
+	return 0;
+}
