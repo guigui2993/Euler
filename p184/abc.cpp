0ff7fc07d4342523f0fdf522154920a74c6e6b22
Guillaume Lempereur <guigui2993@users.noreply.github.com>
2025-01-31 16:09:17 +0100

    bug with total

--- p184/abc.cpp
+++ p184/abc.cpp
@@ -3,6 +3,7 @@
 #include <format>
 #include <chrono>
 #include <ctime>
+#include <cmath>
 #include <ratio>
 #include <set>
 #include <iterator>
@@ -38,16 +39,18 @@ int main(int args, char **argv){
 	 * size: 10: 1101232 ok
 	 * size: 11: 2039688 ok
 	 * size: 15: 13638120 ok
-	 * size: 20: x (77188968 + ) 1.22s 0.6s 0.29855s 0.33 0.27s
+	 * size: 20: x (77188968 + ) 1.22s 0.6s 0.29855s 0.33 0.27s 0.19s
 	 * size: 25: 300344032 TBC (1.36604s)
 	 * size: 30: 913888744
 	 * size: 33: 1630362432 (9.54s)
 	 * size: 35: overflow :D
+	 * size: 50: overflow :D (41s)
 	 * size: 105: 
 	 *
 	*/
 
 	int lim = 2, cnt = 0, c = 0;
+	int c1 = 0, c2 = 0;
 	if(args==2)
 		lim = std::atoi(argv[1]);
 
@@ -76,9 +79,12 @@ for(int ax=1-lim;ax<0;++ax){
 			for(int by=byMin;by<lim;++by){
 				if(dst(bx, by, lim) || (ax*ax)+(ay*ay) == (bx*bx)+(by*by)) // d(B) < d(A)
 					continue;
-				int ABxAO = (bx-ax)*-ay+ax*(by-ay); // AB x AO
+				if((ax*ax)+(ay*ay) < (bx*bx)+(by*by) && (bx >= 0 || by > 0))
+					continue;
+				int ABxAO = ax*by-ay*bx; // AB x AO
 				if(ABxAO==0)
 					continue;
+				//int cyMin = 1-lim;
 				for(int cx=std::max(1,bx);cx<lim;++cx){
 					for(int cy=1-lim;cy<lim;++cy){
 						if(cx==bx && cy <= by)
@@ -88,19 +94,6 @@ for(int ax=1-lim;ax<0;++ax){
 						if((cx*cx)+(cy*cy) > (bx*bx)+(by*by) && (cx*cx)+(cy*cy) > (ax*ax)+(ay*ay))
 							continue;
 
-						if((ax*ax)+(ay*ay)>(bx*bx)+(by*by)){
-							if((ax*ax)+(ay*ay) > (cx*cx)+(cy*cy)){
-								if(ax>=0 || ay >0) // A > B&C
-									continue;
-							}else
-								continue;// C > A&B
-						}else{ // B > A
-							if((bx*bx)+(by*by) > (cx*cx)+(cy*cy)){
-								if(bx>=0 || by >0) // B > A&C
-									continue;
-							}else
-								continue;// C > A&B
-						}
 						if(((cx-ax)*-ay+ax*(cy-ay)) * ABxAO  >= 0) // AC x AO &&  AB x AO opposite sign != 0
 							continue;
 						if(((ax-cx)*-cy+cx*(ay-cy)) * ((bx-cx)*-cy+cx*(by-cy)) >= 0) // CA x CO &&  CB x CO opposite sign != 0
@@ -114,35 +107,48 @@ for(int ax=1-lim;ax<0;++ax){
 	for(int ay=1;ay<lim;++ay){ // A in the top left quadrant => d(B) > d(A) & B in botton left quadrant
 		if(dst(ax, ay, lim))
 			break;
-		for(int bx=-1;bx>=ax;--bx){
-			int byMin = 1-lim;
-			if(bx==ax)
-				byMin = ay+1;
-			for(int by=0;by>=byMin;--by){
+		for(int bx=-1;bx>ax;--bx){ //bx>ax otherwise impossible to have B < A
+			for(int by=0;by>=1-lim;--by){
 				if(dst(bx, by, lim))
-					break;//continue;
+					break;
 				if((ax*ax)+(ay*ay) >= (bx*bx)+(by*by)) // d(B) > d(A)
 					continue;
 				for(int cx=1;cx<lim;++cx){
-					for(int cy=1-lim;cy<lim;++cy){
-						if(cx==bx && cy <= by)
-							continue;
-						if(dst(cx, cy, lim) || (bx*bx)+(by*by) == (cx*cx)+(cy*cy) || (ax*ax)+(ay*ay) == (cx*cx)+(cy*cy)) // d(C) < d(B)
-							continue;
-						if((cx*cx)+(cy*cy) > (bx*bx)+(by*by) && (cx*cx)+(cy*cy) > (ax*ax)+(ay*ay))
-							continue;
-						//
-						if((bx*bx)+(by*by) > (cx*cx)+(cy*cy)){
-							if(bx>=0 || by >0) // B > A&C
-								continue;
-						}else
-							continue;// C > A&B
-						//if((cx-ax)*-ay+ax*(cy-ay) >= 0) // AC x AO &&  AB x AO opposite sign != 0 ABxAO >0
-						if(cx*-ay+ax*cy >= 0) // AC x AO &&  AB x AO opposite sign != 0 ABxAO >0
-							continue;
-						if(((ax-cx)*-cy+cx*(ay-cy)) * ((bx-cx)*-cy+cx*(by-cy)) >= 0) // CA x CO &&  CB x CO opposite sign != 0
+					int cyMin = ay*cx/ax;
+					int cyMax = cx*by/bx;
+					if(cyMin*ax==ay*cx)
+						cyMin++;
+					if(cyMax*bx==by*cx)
+						cyMax--;
+					int cySqtMax = std::sqrt(bx*bx+by*by-cx*cx);
+					if(cySqtMax*cySqtMax == bx*bx+by*by-cx*cx)
+						cySqtMax--;
+					cyMin = std::max(cyMin, -cySqtMax);
+					cyMax = std::min(cyMax, cySqtMax);
+					int cySqtA = std::sqrt(ax*ax+ay*ay-cx*cx);
+					
+					int tot = cyMax-cyMin+1;
+					if(tot > 0){
+						c1 += tot;
+						//cnt += tot;
+						//c += tot;
+						if(cySqtA*cySqtA == ax*ax+ay*ay-cx*cx && ax*ax+ay*ay-cx*cx >= 0){
+							if(cySqtA <=cyMax && cySqtA >= cyMin){
+								//cnt--;c--;
+								c1--;
+							}
+							if(-cySqtA <=cyMax && -cySqtA >= cyMin){
+								//cnt--;c--;
+								//c1--;
+							}
+						}
+					}
+					
+					for(int cy=cyMin;cy<=cyMax;++cy){ //ay*cx/ax < cy < cx*by/bx
+						if((ax*ax)+(ay*ay) == (cx*cx)+(cy*cy)) // d(C) < d(B)
 							continue;
 						cnt++; c++;
+						c2++;
 					}
 				}
 			}
@@ -273,6 +279,7 @@ for(int ax=0;ax>-lim;--ax){
 	std::cout << "Duration: " << time_span.count() << "s" << std::endl;
 	std::cout << "size "<<  lim << " : " << cnt << "  " << cnt*4 << std::endl;
 	std::cout << "A<B<C "<<  lim << " : " << c << "  " << c*4 << std::endl;
+	std::cout << "diff "<< c2 << "  " << c1 << std::endl;
 
 return 0;
 }
